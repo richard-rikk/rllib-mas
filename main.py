8814acb54cd638b16c5c92de5ece2e34e7ef9336
Richard Rikk <rikk.richard@gmail.com>
2022-11-02 08:29:06 +0100

    Added PPO algorithm to the training module.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from simulation import Simulation
 from model      import Training
 
-# sim2 = Simulation(config={"height":5, "width":5, "obj_cnt":0, "obst_cnt":0, "tar_cnt":1, "max_step":10})
+# sim2 = Simulation(config={"height":10, "width":10, "obj_cnt":10, "obst_cnt":10, "tar_cnt":10, "max_step":10})
 # for i in range(10):
 #     print(sim2.step(action=3))
 #     print(sim2.observation_space.map)
 #     print("\n")
 
 Training(
-    epochs=1_000,
+    epochs=2,
     num_workers=4,
-    buffer_size=1024,
-    gpus=1,
+    buffer_size=2048,
+    gpus=0,
     sim_height=10,
     sim_width=10,
     sim_objs=10,
--- model/training.py
+++ model/training.py
@@ -1,7 +1,8 @@
+from distutils.command.config import config
 import os
 import ray
 import torch
-from ray.rllib.algorithms import apex_dqn
+from ray.rllib.algorithms import apex_dqn, ppo
 from simulation           import Simulation
 from utils                import Plot
 
@@ -37,32 +38,14 @@ def Training(
     ray.init()
 
     # Set the config.
-    config = apex_dqn.ApexDQNConfig()
-    replay_config = config.replay_buffer_config.update({"capacity": buffer_size, "learning_starts":buffer_size})
-
-    config.training(
-        num_atoms=2, v_min=0., v_max=10.,
-        noisy=True, dueling=True, hiddens=[128,128],
-        double_q=True, n_step=5, 
-        replay_buffer_config=replay_config
+    config = ppoConfig(
+        num_workers=num_workers, gpus=gpus, buffer_size=buffer_size, max_step=max_step,
+        sim_height=sim_height, sim_width=sim_width, sim_objs=sim_objs, sim_obs=sim_obs, sim_tars=sim_tars
     )
-    config.resources(num_gpus=gpus)
-    config.framework(framework="torch")
-    config.environment(
-        env=Simulation, 
-        env_config={
-            "height":sim_height,
-            "width":sim_width,
-            "obj_cnt":sim_objs,
-            "obst_cnt":sim_obs,
-            "tar_cnt":sim_tars,
-            "max_step":max_step,
-            }
-        )
-    config.num_workers = num_workers
 
     # Set the model
-    model = apex_dqn.ApexDQN(env=Simulation, config=config)
+    # model = apex_dqn.ApexDQN(env=Simulation, config=config)
+    model = ppo.PPO(config=config)
 
     # Set the training loop
     epoch                 = epochs
@@ -90,6 +73,62 @@ def Training(
 
     # Shutdown the ray instance.
     ray.shutdown()
+
+def dqnConfig(num_workers:int, gpus:int, buffer_size:int, max_step:int,
+    sim_height:int, sim_width:int, sim_objs:int, sim_obs:int, sim_tars:int) -> apex_dqn.ApexDQNConfig:
+    """
+    Creates a dqn config.
+    """
+    config = apex_dqn.ApexDQNConfig()
+    replay_config = config.replay_buffer_config.update({"capacity": buffer_size, "learning_starts":buffer_size})
+
+    config.training(
+        num_atoms=2, v_min=0., v_max=10.,
+        noisy=True, dueling=True, hiddens=[128,128],
+        double_q=True, n_step=5, 
+        replay_buffer_config=replay_config
+    )
+    config.resources(num_gpus=gpus)
+    config.framework(framework="torch")
+    config.environment(
+        env=Simulation, 
+        env_config={
+            "height":sim_height,
+            "width":sim_width,
+            "obj_cnt":sim_objs,
+            "obst_cnt":sim_obs,
+            "tar_cnt":sim_tars,
+            "max_step":max_step,
+            }
+        )
+    config.num_workers = num_workers
+
+    return config
+
+def ppoConfig(num_workers:int, gpus:int, buffer_size:int, max_step:int,
+    sim_height:int, sim_width:int, sim_objs:int, sim_obs:int, sim_tars:int) -> ppo.PPOConfig:
+    """
+    Returns a PPO config.
+    """
+
+    config = ppo.PPOConfig()
+
+    config.resources(num_gpus=gpus)
+    config.framework(framework="torch")
+    config.environment(
+        env=Simulation, 
+        env_config={
+            "height":sim_height,
+            "width":sim_width,
+            "obj_cnt":sim_objs,
+            "obst_cnt":sim_obs,
+            "tar_cnt":sim_tars,
+            "max_step":max_step,
+            }
+        )
+    config.num_workers = num_workers
+
+    return config
 
 def Save(model:torch.nn.Module, name:str,loc:str) -> None:
     """
